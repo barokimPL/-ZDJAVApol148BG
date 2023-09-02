1229a794d0ad7e71e8bdf10fccea07cd2d2e1daf
barokimPL <baroxim@gmail.com>
2023-09-02 15:45:57 +0200

    If that's not it I give up

--- Pol148/src/main/java/SimpleTuple/Main.java
+++ Pol148/src/main/java/SimpleTuple/Main.java
@@ -3,7 +3,7 @@ package SimpleTuple;
 public class Main {
 
     public static void main(String[] args) {
-        SimpleTuple<Integer> t = new SimpleTuple<>(8, 2);
+        SimpleTuple<Integer> t = new SimpleTuple<>(8, 12);
 //        SimpleTuple2<Animal> t = new SimpleTuple<>(
 //                new Animal(1, "Alice", "Horse"),
 //                new Animal(2, "Romuald", "Ass"))
