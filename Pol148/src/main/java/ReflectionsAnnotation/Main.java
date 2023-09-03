c465853411b1716c1e49ed02e5c690fe173ac94c
barokimPL <baroxim@gmail.com>
2023-09-03 14:14:17 +0200

    Task 03 finished

--- Pol148/src/main/java/ReflectionsAnnotation/Main.java
+++ Pol148/src/main/java/ReflectionsAnnotation/Main.java
@@ -6,8 +6,6 @@ import java.lang.reflect.Method;
 public class Main {
     public static void main(String[] args) {
         Phone phone = new Phone("Y5", "Huawei", 2022);
-//        System.out.println(phone.getAge(2025));
-//        System.out.println(phone.butWhatIfItWasSamsung());
 
         getRuntimeInfo();
     }
---
+++ Pol148/src/main/java/ReflectionsAnnotation/Task03.java
@@ -0,0 +1,43 @@
+package ReflectionsAnnotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task03 {
+    public static void main(String[] args) {
+        Class<Phone> cl = Phone.class;
+        Constructor<?> constructor = cl.getConstructors()[0];
+
+        try {
+            Phone phone = (Phone) constructor.newInstance("Samsung", "Galaxy S5", 2022);
+            List<Method> method = findMethodsWithAnnotation(phone.getClass(),"SuperMethod");
+            System.out.println("Support's going to last until the year " + method.get(0).invoke(phone, 4));
+        } catch (InstantiationException e) {
+            System.out.println("AaaaaAaAAAA!");
+        } catch (IllegalAccessException e) {
+            System.out.println("Ratunku!");
+        } catch (InvocationTargetException e) {
+            System.out.println("Au secours!");
+        }
+    }
+
+    private static List<Method> findMethodsWithAnnotation(Class<? extends Phone> cl, String annotationName) {
+        List<Method> methods = new ArrayList<>();
+
+        for (Method method : cl.getDeclaredMethods()){
+
+            for (Annotation anno : method.getAnnotations()){
+               String annoName = anno.annotationType().getName();
+               if (annoName.contains(annotationName))
+                   methods.add(method);
+            }
+
+        }
+        return methods;
+    }
+
+}
